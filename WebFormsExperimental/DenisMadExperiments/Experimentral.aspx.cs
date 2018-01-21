b976b656bd87c0ea916d232a60884bb5ced78140
NielsTyler <dvtsvetkov@gmail.com>
2018-01-21 16:58:05 +0300

    Add my experimental page2

---
+++ WebFormsExperimental/DenisMadExperiments/Experimentral.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormsExperimental.DenisMadExperiments
+{
+    public partial class Experimentral : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                MultiView1.ActiveViewIndex = 0;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            MultiView1.ActiveViewIndex = MultiView1.ActiveViewIndex == 1 ? 0 : 1;
+        }
+    }
+}

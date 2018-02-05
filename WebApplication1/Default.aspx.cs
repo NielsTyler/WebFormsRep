2b04132629175edab14a4dea1e3d365bc6dcc4ce
NielsTyler <dvtsvetkov@gmail.com>
2018-02-05 15:42:56 +0300

    Add correct WEb forms project

---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {            
+        }
+    }
+}
---
+++ WebApplication1/Den/DenForm1.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication1.Models;
+
+namespace WebApplication1.Den
+{
+    public partial class DenForm1 : System.Web.UI.Page
+    {
+        int TestField = 0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                GuestResponse rsvp = new GuestResponse();
+
+                if (TryUpdateModel(rsvp, new FormValueProvider(ModelBindingExecutionContext)))
+                {
+                    ResponseRepository.GetRepository().AddResponse(rsvp);
+                }
+            }
+            else
+            {
+                ViewState["testViewState"] = 3;
+            }
+        }
+
+        protected void DenCommand(object sender, CommandEventArgs e)
+        {
+            ViewState["testViewState"] = (int)ViewState["testViewState"] + 1;
+            //testLabel.Text = (++TestField).ToString();
+            testLabel.Text += ViewState["testViewState"];
+
+            testHiddenField.Value = "What are hell is going on here?";
+        }
+
+        protected void FileUpload1_Load(object sender, EventArgs e)
+        {
+            Debug.Write("asd");
+        }
+    }
+}

4c1b6ddc6db92503170b6d9d3e22c5f76388f263
NielsTyler <dvtsvetkov@gmail.com>
2018-02-18 20:59:39 +0300

    Add method1 and comment 1st

--- WebApplication1/Default.aspx.cs
+++ WebApplication1/Default.aspx.cs
@@ -12,7 +12,12 @@ namespace WebApplication1
     public partial class _Default : Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {            
+        {  
+		//Test string from 1st user          
         }
+
+	private String Method1()
+	{
+	}
     }
 }

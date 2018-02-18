9accc9e7af4f159f630c9b4d865a0389e3929ff0
NielsTyler <dvtsvetkov@gmail.com>
2018-02-18 17:50:08 +0300

    Second guy commit

--- WebApplication1/About.aspx.cs
+++ WebApplication1/About.aspx.cs
@@ -11,7 +11,11 @@ namespace WebApplication1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+		//Hello from second guy!
         }
+
+	private int FirstPersonMethod()
+	{
+	}
     }
 }
